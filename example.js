06d27a3dacbac1694933c78e61e31a2bd1d7cdd7
grantmatthews18 <grant.d.matthews@gmail.com>
2025-06-18 11:50:50 -0500

    Adding Examples

--- example.js
+++ example.js
@@ -1,7 +1,21 @@
 window.optimizelyFX.push({
+    'type': 'init',
+    'sdkKey': '2a6qXYH7XRi9DQezV4amE',
+    'userId': '12345',
+    'attributes': {
+        'age': 30
+    },
+})
+
+window.optimizelyFX.push({
     'type': 'user',
     'userId': '12345',
     'attributes': {
         'age': 30
     }
+})
+
+window.optimizelyFX.push({
+    'type': 'event',
+    'eventName': 'Test Event'
 })
